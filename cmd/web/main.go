a94ba9e3d8bd49d3d7247d90b5a2d8a5748a6ca8
Peter Nirschl <peter.nirschl@gmail.com>
2021-02-27 21:48:28 +0100

    Chapter 3.1 - add command line argument for address

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	serverAddress := flag.String("address", ":8080", "Network address (and port) of the server")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -14,7 +18,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on port 8080")
-	err := http.ListenAndServe(":8080", mux)
+	log.Printf("Starting server on %s", *serverAddress)
+	err := http.ListenAndServe(*serverAddress, mux)
 	log.Fatal(err)
 }
