a674b2cefed9935019c732cf43b76231e4fdd797
Peter Nirschl <peter.nirschl@gmail.com>
2021-03-07 11:09:42 +0100

    Chapter 11.6 check authentication / authorization

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,23 +9,24 @@ import (
 
 func (app *application) routes() http.Handler {
 	defaultMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	dynamicMiddleware := alice.New(app.session.Enable)
+	sessionMiddleware := alice.New(app.session.Enable)
+	authenticatedMiddleware := sessionMiddleware.Append(app.requireAuthentication)
 
 	// HTTP message routing
 	router := mux.NewRouter()
 
 	// register HTML routes (snippet)
-	router.Handle("/", dynamicMiddleware.ThenFunc(app.htmlHome)).Methods("GET")
-	router.Handle("/snippet/create", dynamicMiddleware.ThenFunc(app.htmlCreateSnippetForm)).Methods("GET")
-	router.Handle("/snippet/create", dynamicMiddleware.ThenFunc(app.htmlCreateSnippet)).Methods("POST")
-	router.Handle("/snippet/{id}", dynamicMiddleware.ThenFunc(app.htmlShowSnippet)).Methods("GET")
+	router.Handle("/", sessionMiddleware.ThenFunc(app.htmlHome)).Methods("GET")
+	router.Handle("/snippet/create", authenticatedMiddleware.ThenFunc(app.htmlCreateSnippetForm)).Methods("GET")
+	router.Handle("/snippet/create", authenticatedMiddleware.ThenFunc(app.htmlCreateSnippet)).Methods("POST")
+	router.Handle("/snippet/{id}", sessionMiddleware.ThenFunc(app.htmlShowSnippet)).Methods("GET")
 
 	// register HTML routes (user)
-	router.Handle("/user/signup", dynamicMiddleware.ThenFunc(app.htmlSignupUserForm)).Methods("GET")
-	router.Handle("/user/signup", dynamicMiddleware.ThenFunc(app.htmlSignupUser)).Methods("POST")
-	router.Handle("/user/login", dynamicMiddleware.ThenFunc(app.htmlLoginUserForm)).Methods("GET")
-	router.Handle("/user/login", dynamicMiddleware.ThenFunc(app.htmlLoginUser)).Methods("POST")
-	router.Handle("/user/logout", dynamicMiddleware.ThenFunc(app.htmlLogoutUser)).Methods("POST")
+	router.Handle("/user/signup", sessionMiddleware.ThenFunc(app.htmlSignupUserForm)).Methods("GET")
+	router.Handle("/user/signup", sessionMiddleware.ThenFunc(app.htmlSignupUser)).Methods("POST")
+	router.Handle("/user/login", sessionMiddleware.ThenFunc(app.htmlLoginUserForm)).Methods("GET")
+	router.Handle("/user/login", sessionMiddleware.ThenFunc(app.htmlLoginUser)).Methods("POST")
+	router.Handle("/user/logout", authenticatedMiddleware.ThenFunc(app.htmlLogoutUser)).Methods("POST")
 
 	// register API routes (JSON)
 	// NOTES these routes have nothing to do with the book
